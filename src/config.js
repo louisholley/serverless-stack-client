1bcd778be9c1c6ba5d33df5f5ba63b20b6b686e6
louis holley <louisholley@me.com>
2020-01-07 09:36:59 +0000

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "test.notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://0ngc2pj3hb.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_SaNCNPusV",
+    APP_CLIENT_ID: "6t7t9jg31hf94m8aikoukkjf4f",
+    IDENTITY_POOL_ID: "eu-west-2:0ed3c6ff-979e-4a9c-ae58-1880d878387f"
+  }
+};
